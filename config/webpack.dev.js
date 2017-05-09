37799330c72abb98ad706cc00b349393ee58226f
Vlad Rindevich <rindevich.vs@gmail.com>
2017-05-09 14:42:42 +0300

    build: initial commit

---
+++ config/webpack.dev.js
@@ -0,0 +1,24 @@
+const config = require('./webpack.common');
+const paths = require('./paths');
+
+module.exports = Object.assign({}, config, {
+  output: Object.assign({}, config.output, {
+    filename: 'lazyx.js',
+    path: paths.build,
+  }),
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        include: paths.src,
+        loader: 'awesome-typescript-loader',
+        options: {
+          cacheDirectory: paths.cache,
+          configFileName: paths.tsconfig,
+          forkChecker: true,
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+});
