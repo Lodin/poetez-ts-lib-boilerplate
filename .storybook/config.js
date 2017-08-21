167f210a0eaff005ac0d5c2bc37add15e5a7eb69
Vlad Rindevich <rindevich.vs@gmail.com>
2017-08-21 14:16:41 +0300

    refactor(config): remove unnecessary normalize.css dependency & remove theme.css

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,9 +3,6 @@ import { setOptions } from '@storybook/addon-options';
 import JSXAddon from 'storybook-addon-jsx';
 
 function loadStories() {
-  require('normalize.css/normalize.css');
-  require('../src/core/theme.css');
-
   const context = require.context("../src/components", true, /\.story\.tsx$/);
   for (const key of context.keys()) {
     context(key);
